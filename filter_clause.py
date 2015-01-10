7bbf934f451227e44bfd4de34804cedbf46b88e0
elssar <elssar@altrawcode.com>
2015-01-10 10:49:23 +0530

    Added tests and refactored

---
+++ filter_clause.py
@@ -0,0 +1,39 @@
+import datetime
+
+
+class FilterClause(object):
+
+    FILTER_MAP = {
+        "eq": "=",
+        "neq": "!=",
+        "lt": "<",
+        "lte": "<=",
+        "gt": ">",
+        "gte": ">=",
+    }
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def _clean_value(cls, value):
+        if isinstance(value, (str, unicode)):
+            return "\"{0}\"".format(value)
+        elif isinstance(value, (datetime.datetime, datetime.date, datetime.time)):
+            return "\"{0}\"".format(value)
+        elif isinstance(value, (int, float)):
+            return value
+
+    def to_query(self):
+        filter_key = self.key.split("__")
+        if len(filter_key) > 1:
+            filter_operator = self.FILTER_MAP.get(filter_key[-1])
+            if not filter_operator:
+                filter_operator = "="
+            else:
+                filter_key = filter_key[:-1]
+        else:
+            filter_operator = "="
+
+        return u'{}{}{}'.format(filter_key[0], filter_operator,
+                                self._clean_value(self.value))
---
+++ tests.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import unittest
+
+from storm import Model
+from filter_clause import FilterClause
+
+class TestModel(Model):
+    table = "test"
+
+
+class StormTest(unittest.TestCase):
+
+    def test_returns_query_for_eqality(self):
+        query = TestModel.filter(name='foo')
+
+        expected_query = 'SELECT * FROM test WHERE name="foo";'
+        assert query == expected_query
+
+    def test_returns_query_for_equality_with_and(self):
+        query = TestModel.filter(name='foo', age=24)
+
+        expected_query = 'SELECT * FROM test WHERE age=24 AND name="foo";'
+        assert query == expected_query
+
+    def test_returns_query_for_ineqality(self):
+        query = TestModel.filter(name__neq='foo')
+
+        expected_query = 'SELECT * FROM test WHERE name!="foo";'
+        assert query == expected_query 
+
+    def test_returns_query_for_less_than(self):
+        query = TestModel.filter(age__lt=25)
+
+        expected_query = 'SELECT * FROM test WHERE age<25;'
+        assert query == expected_query 
+
+    def test_returns_query_for_greater_than(self):
+        query = TestModel.filter(age__gt=25)
+
+        expected_query = 'SELECT * FROM test WHERE age>25;'
+        assert query == expected_query 
+
+
+class FilterClauseTest(unittest.TestCase):
+    def test_should_get_query(self):
+        key = 'name'
+        clause = FilterClause(key, 'foo')
+
+        assert clause.to_query() == '{}={}'.format(key, '"foo"')
+
+    def test_should_create_from_key_and_value(self):
+
+        query = FilterClause('name__eq', 'foo').to_query()
+
+        assert query == 'name="foo"'
